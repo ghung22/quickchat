2ce2a5afbd206b3993fe028a1dbeed29aac0b1ea
18127044 <giahungnguyen999@gmail.com>
2020-12-31 21:13:20 +0700

    Fix listenning error loop + Fix Close server button

--- QuickchatServer.java
+++ QuickchatServer.java
@@ -2,7 +2,5 @@ public class QuickchatServer {
     public static void main(String[] args) {
         Server s = new Server();
         s.launch();
-        // TODO: while loop in both client and server (error while listening to
-        // server/client)
     }
 }
